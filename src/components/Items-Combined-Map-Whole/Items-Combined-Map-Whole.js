ef469a540c9f6d9dd235ab74ce4fee159c20560e
Patryk <mastasinqwer@gmail.com>
2020-09-19 11:34:48 +0200

    End items with tooltip all

--- src/components/Items-Combined-Map-Whole/Items-Combined-Map-Whole.js
+++ src/components/Items-Combined-Map-Whole/Items-Combined-Map-Whole.js
@@ -1,48 +1,45 @@
 import React from 'react'
 import './Items-Combined-Map-Whole.css'
 
-const ItemsCombinedMapWhole = ( {ItemsJson, itemId} ) => {
+import ItemCard from '../Item-Card/Item-Card';
+
+const ItemsCombinedMapWhole = ({ ItemsJson, itemId }) => {
     return (
         <ul className='Items-Combined-Map-Whole-Container'>
-            {ItemsJson.map( ({id, name, stats, description, recipe}) => {
+            {ItemsJson.map(({ id, recipe }) => {
                 const image = id < 10 ? `0${id}` : id;
-                if(id !== itemId) return false;
+                if (id !== itemId) return false;
                 return (
-                    <li 
+                    <div
+                        className='Items-Combined-Map-Component-Item-Block'
                         key={id}
-                        className='Items-Combined-Map-Whole-Item'
                     >
-                        {recipe.map( (id, fakeIndex) => {
+                        {recipe.map((id, fakeIndex) => {
                             // fakeIndex bcs some items are built with the same item
                             return (
-                                <div 
-                                    key= {fakeIndex}
-                                    className='Items-Combined-Map-Whole-Item-Components'
-                                >
-                                    <img 
-                                        src= { process.env.PUBLIC_URL + `/assets/images/items/0${id}.webp`} 
-                                        onError={e => {
-                                            e.target.src = `${process.env.PUBLIC_URL + `/assets/images/items/0${id}.png`}`
-                                        }}
-                                        alt=''
-                                    />
-                                    {recipe[1] ? <div>=</div> : <div>+</div>}
-                                </div>
+                                <React.Fragment key={fakeIndex}>
+                                    <li className='Items-Combined-Map-Whole-Item'>
+                                        <ItemCard
+                                            itemId={id}
+                                            image={`0${id}`}
+                                            imageOnError={`0${id}`}
+                                        />
+                                    </li>
+                                    {fakeIndex === 0 ? <div>+</div> : null}
+                                </React.Fragment>
                             )
                         })}
-                        <div className='Tool-Tip'>
-                            <p> { name } </p>
-                            <p> { stats } </p>
-                            <p> { description } </p>
-                        </div>
-                        <img 
-                            src= { process.env.PUBLIC_URL + `/assets/images/items/${image}.webp`}
-                            onError={e => {
-                                e.target.src = `${process.env.PUBLIC_URL + `/assets/images/items/${image}.png`}`
-                            }}
-                            alt= { name }
-                        />
-                    </li>
+
+                        <div>=</div>
+
+                        <li className='Items-Combined-Map-Whole-Item'>
+                            <ItemCard
+                                itemId={image}
+                                image={image}
+                                imageOnError={image}
+                            />
+                        </li>
+                    </div>
                 )
             })}
         </ul>
